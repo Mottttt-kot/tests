a41054da6d3243b1cd62f39a0251328d81e11561
programmistotboga <matvolodkin@gmail.com>
2020-03-14 11:59:45 +0900

    vcvccvcvc

--- Projectgame/Assets/Materials/c.cs
+++ Projectgame/Assets/Materials/c.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class а : MonoBehaviour
+public class c : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
