e5f562ce68bee025fd7ff39b0c256111f7e50ec6
programmistotboga <matvolodkin@gmail.com>
2020-03-15 12:36:47 +0900

    Vot tut

---
+++ mobileees/Assets/Low Poly FPS Pack - Free (Sample)/Components/Scripts/Weapon_Scripts/click.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class click : MonoBehaviour {
+	
+	public Button yourButton;
+	public Camera gunCamera;
+	[Header("Gun Camera Options")]
+	public float aimFov = 15.0f;
+	public float fovSpeed = 15.0f;
+	private bool isAiming;
+
+	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+
+	void TaskOnClick(){
+					gunCamera.fieldOfView = Mathf.Lerp (gunCamera.fieldOfView,
+				aimFov, fovSpeed * Time.deltaTime);
+			
+			isAiming = true;
+
+	}
+}
